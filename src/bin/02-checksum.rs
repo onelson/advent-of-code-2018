273c429cb9076f6ee901ee8606e9efef28797c4b
Owen Nelson <onelson@gmail.com>
2018-12-03 23:39:39 -0800

    gold for day 2.

--- src/bin/02-checksum.rs
+++ src/bin/02-checksum.rs
@@ -17,23 +17,33 @@ fn has_exact_count(count: usize, s: &str, chars: &HashSet<char>) -> usize {
     }
 }
 
+fn similar(a: &str, b: &str) -> bool {
+    a.chars().zip(b.chars()).filter(|(c1, c2)| c1 != c2).count() == 1
+}
+
 fn main() {
     let fp = env::args().skip(1).next().expect("file path");
     let f = File::open(fp).unwrap();
     let reader = BufReader::new(f);
 
-    let ret: (usize, usize) = reader
-        .lines()
-        .map(|x| {
-            let input = x.unwrap();
-            let chars: HashSet<_> = input.chars().collect();
-            (
-                has_exact_count(2, &input, &chars),
-                has_exact_count(3, &input, &chars),
-            )
-        }).fold((0, 0), |acc, t| (acc.0 + t.0, acc.1 + t.1));
+    let ids: Vec<_> = reader.lines().map(|x| x.unwrap()).collect();
 
-    println!("{} * {} = {}", ret.0, ret.1, ret.0 * ret.1);
+    let mut pair: (String, String) = ("".to_string(), "".to_string());
+    'outer: for id1 in &ids {
+        for id2 in &ids {
+            if similar(id1, id2) {
+                pair = (id1.to_string(), id2.to_string());
+                break 'outer;
+            }
+        }
+    }
+    let ret: String = pair
+        .0
+        .chars()
+        .zip(pair.1.chars())
+        .filter_map(|(a, b)| if a == b { Some(a) } else { None })
+        .collect();
+    println!("{}", ret);
 }
 
 #[cfg(test)]
@@ -58,5 +68,20 @@ mod tests {
     fn test_match_quads() {
         let chars: HashSet<_> = INPUT.chars().collect();
         assert_eq!(1, has_exact_count(4, INPUT, &chars));
+    }
+
+    #[test]
+    fn test_similars() {
+        assert!(similar("fghij", "fguij"));
+        assert!(similar("fghij", "fghiy"));
+        assert!(similar("pghij", "fghij"));
+    }
+
+    #[test]
+    fn test_disimilars() {
+        // two different
+        assert!(!similar("fghij", "fguiy"));
+        // identicals are not similars
+        assert!(!similar("fghij", "fghij"));
     }
 }
